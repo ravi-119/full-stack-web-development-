89032c05098d543befae037cbd04867bd1b54dbe
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-04-20 02:17:02 +0530

    Add files via upload

---
+++ DOM Assignment/Event Listener/DoubleClick/script.js
@@ -0,0 +1,6 @@
+const boxElement = document.getElementById("box");
+
+// Add a click Event Listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
+});
