70a2c4a5840aeb4b7d670b0700669d5df896716e
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-04-20 02:19:19 +0530

    Add files via upload

---
+++ DOM Assignment/Event Listener/MouseOver/script.js
@@ -0,0 +1,18 @@
+const boxElement = document.getElementById("box");
+
+//add a event Listener to the button
+boxElement.addEventListener("mouseover", function () {
+  alert(
+    "you entered in Restricted Area 🙅 (mouse over event is working properly)"
+  );
+});
+
+
+// const boxElement = document.getElementById("box");
+//
+// // add a mouseover event listener to the element
+// boxElement.addEventListener("mouseover", function () {
+//   alert(
+//     "you entered in Restricted Area 🙅 (mouse over event is working properly)"
+//   );
+// });
