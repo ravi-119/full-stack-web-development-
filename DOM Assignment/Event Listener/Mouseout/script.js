4a445fb104cf455a60bcb80af71f82197528b7cc
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-04-21 01:21:46 +0530

    Add files via upload

---
+++ DOM Assignment/Event Listener/Mouseout/script.js
@@ -0,0 +1,14 @@
+const boxElement = document.getElementById("box")
+
+// adding a eventlistener
+boxElement.addEventListener("mouseout", function (){
+  alert("you left the safe zone 🙅 (mouse out event working properly");
+});
+
+
+// const boxElement = document.getElementById("box");
+//
+// // add a mouseout event listener to the element
+// boxElement.addEventListener("mouseout", function () {
+//   alert("you left the safe zone 🙅 (mouse out event working properly)");
+// });
