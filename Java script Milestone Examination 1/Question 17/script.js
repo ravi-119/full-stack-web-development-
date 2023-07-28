77c3dce88e828fe1d6ade93852d688208c0f9e17
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-07-29 01:50:22 +0530

    Add files via upload

--- Java script Milestone Examination 1/Question 17/script.js
+++ Java script Milestone Examination 1/Question 17/script.js
@@ -1,1 +1,19 @@
+let deposit  = (user, amount) => {
+    user.balance += amount
+}
+
+let withdraw = (user, amount) => {
+    if (user.balance >= amount) {
+        user.balance -= amount
+        
+    } else {
+        console.log("Insufficient Balance");
+    }
+
+}
+
+let user = { nmae: "Ravi Kumar Yadav", balance: 200}
+// deposit(user, 100)
+withdraw(user, 100)
+console.log(user);
 
