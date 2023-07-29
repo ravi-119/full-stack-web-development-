96ec88599a98340a57cccbdefbad5844964e3a7a
Ravi Kumar Yadav <122007962+ravi-119@users.noreply.github.com>
2023-07-29 15:30:25 +0530

    Add files via upload

---
+++ Java script Milestone Examination 1/Question 22/script.js
@@ -0,0 +1,42 @@
+const result = document.getElementsByClassName('result');
+const button = document.getElementById('btn');
+const randomColor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#" ;
+    for (let i = 0; i < 6; i++) {
+        
+       cons = cons + val [Math.floor(Math.random() * 11)];
+    }
+    return cons;
+};
+
+console.log(randomColor());
+
+
+function changeRandomColor() {
+    result[0].style.backgroundColor = randomColor();
+    result[1].style.backgroundColor = randomColor();
+}
+
+
+button.addEventListener("click", changeRandomColor);
+
+// const result = document.getElementsByClassName('result');
+// const button = document.getElementById("btn");
+// const randomColor =  () => {
+//     let val = "0123456789ABCDEF";
+//     let cons = "#";
+//     for(let i = 0; i < 6; i++) {
+//         cons = cons + val[Math.floor(Math.random() * 16)];
+//     }
+//     return cons;
+// };
+
+// // console.log(randomColor());
+
+// function changeRandomColor() {
+//     result[0].style.backgroundColor = randomColor();
+//     result[1].style.backgroundColor = randomColor();
+// }
+
+// button.addEventListener("click", changeRandomColor);
